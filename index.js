2f15436012019f26de40196e91058151fef50a99
Brent Labasan <BrentLabasan@gmail.com>
2018-04-04 10:43:37 -0700

    copy pasted validation code from front end | TODO remove .setState s

--- index.js
+++ index.js
@@ -49,6 +49,63 @@ app.post('/send/create', function (req, res) {
     res.end("ERROR: A required data field is missing. Ensure provide Destination, TokenName, and Amount data fields.");
   }
 
+          // Step 1: Ensure public address/key is valid.
+        if (StellarSdk.StrKey.isValidEd25519PublicKey(address)) {
+            this.setState({ addressIsValid: true });
+            localStorage.setItem('lastEnteredAddress', address);
+            // console.log("corr")
+            let server = new StellarSdk.Server('https://horizon.stellar.org');
+            server.accounts()
+                .accountId(address)
+                .call().then((r) => {
+                    console.log(r);
+
+                    // console.log(typeof r);
+                    // console.log(Object.getOwnPropertyNames(r));
+                    // console.log(JSON.stringify(r));
+                    // console.log(JSON.parse(JSON.stringify(r)));
+                    let result = JSON.parse(JSON.stringify(r));
+                    // console.log("result.id", result.id);
+                    // console.log("result.balances", result.balances);
+
+                    // alert(result.id);
+                    // Step 2:  Ensure account has at least 4.5 XLM to cover base fee.s
+                    if (result.balances[result.balances.length - 1].balance >= 4.5) {
+                        this.setState({ hasEnoughXlm: true });
+                        // alert("more than 4.5");
+
+                    } else {
+                        this.setState({ hasEnoughXlm: false });
+                        // alert("less than 4.5");
+                    }
+
+                    // Step 3: Ensure account can accept asset. 
+                    let canAcceptToken = false;
+                    result.balances.forEach((b: any) => {
+                        if (b.asset_code) {
+                            // console.log("typeof b.asset_code", typeof b.asset_code);
+                            console.log("compare balances accepted vs. tab's token", this.props.selectedToken.toUpperCase(), b.asset_code.toUpperCase());
+                            if (this.props.selectedToken.toUpperCase() === b.asset_code.toUpperCase()) {
+                                canAcceptToken = true;
+                                // There's no built-in ability to break in forEach. https://stackoverflow.com/a/2641374
+                            }
+                        }
+                    });
+                    console.log(canAcceptToken);
+                    if (canAcceptToken) {
+                        this.setState({ canAcceptToken: true });
+                    } else {
+                        this.setState({ canAcceptToken: false });
+                    }
+
+                });
+        } else { // if query entered into field isn't a valid public key
+            console.log("query entered into field isn't a valid public key");
+            this.setState({ addressIsValid: false, hasEnoughXlm: false, canAcceptToken: false });
+        }
+
+        this.setState({ address: address });
+
   response = {
     TokenName: req.body.TokenName,
     Amount: req.body.Amount,
