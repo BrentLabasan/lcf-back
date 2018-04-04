e3ebe0fec74ed26a6ce0fe2dfb0c50b2a9e41517
Brent Labasan <BrentLabasan@gmail.com>
2018-04-04 11:33:05 -0700

    commented out Postgres stuff

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ var moment = require('moment');
 var StellarSdk = require('stellar-sdk');
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
 
-client.connect();
+// client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  console.log("BEGIN");
-  for (let row of res.rows) {
-    console.log("ROW");
-    console.log(JSON.stringify(row));
-  }
-  console.log("END");
-  client.end();
-});
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   console.log("BEGIN");
+//   for (let row of res.rows) {
+//     console.log("ROW");
+//     console.log(JSON.stringify(row));
+//   }
+//   console.log("END");
+//   client.end();
+// });
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
