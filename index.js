f342d7ec1804c28ccea1b83c79a91c39587d94dd
Brent Labasan <BrentLabasan@gmail.com>
2018-04-04 14:49:32 -0700

    copied send transaction code from https://www.stellar.org/developers/js-stellar-sdk/reference/examples.html#creating-a-payment-transaction

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ app.get('/send/create', function (request, response) {
 //This sends the user information to the path  
 app.post('/sends/create', function (req, res) {
   // console.log(req);
+
   if (!req.body.Destination || !req.body.TokenName || !req.body.Amount) {
-    res.end(req.body.Destination + req.body.TokenName + req.body.Amount + "ERROR: A required data field is missing. SOLUTION: Ensure provide Destination, TokenName, and Amount data fields.");
-    res.sendStatus(400);
+    res.status(404).end(req.body.Destination + req.body.TokenName + req.body.Amount + "ERROR: A required data field is missing. SOLUTION: Ensure provide Destination, TokenName, and Amount data fields.");
   }
 
   const DESTINATION = req.body.Destination.toUpperCase();
@@ -99,6 +99,70 @@ app.post('/sends/create', function (req, res) {
         console.log(canAcceptToken);
         if (canAcceptToken) {
           // Account can accept token.
+
+          // The source account is the account we will be signing and sending from.
+          var sourceSecretKey = 'SAKRB7EE6H23EF733WFU76RPIYOPEWVOMBBUXDQYQ3OF4NF6ZY6B6VLW';
+
+          // Derive Keypair object and public key (that starts with a G) from the secret
+          var sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
+          var sourcePublicKey = sourceKeypair.publicKey();
+
+          var receiverPublicKey = 'GAIRISXKPLOWZBMFRPU5XRGUUX3VMA3ZEWKBM5MSNRU3CHV6P4PYZ74D';
+
+          // Configure StellarSdk to talk to the horizon instance hosted by Stellar.org
+          // To use the live network, set the hostname to 'horizon.stellar.org'
+          var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+          // Uncomment the following line to build transactions for the live network. Be
+          // sure to also change the horizon hostname.
+          // StellarSdk.Network.usePublicNetwork();
+          StellarSdk.Network.useTestNetwork();
+
+          // Transactions require a valid sequence number that is specific to this account.
+          // We can fetch the current sequence number for the source account from Horizon.
+          server.loadAccount(sourcePublicKey)
+            .then(function (account) {
+              var transaction = new StellarSdk.TransactionBuilder(account)
+                // Add a payment operation to the transaction
+                .addOperation(StellarSdk.Operation.payment({
+                  destination: receiverPublicKey,
+                  // The term native asset refers to lumens
+                  asset: StellarSdk.Asset.native(),
+                  // Specify 350.1234567 lumens. Lumens are divisible to seven digits past
+                  // the decimal. They are represented in JS Stellar SDK in string format
+                  // to avoid errors from the use of the JavaScript Number data structure.
+                  amount: '350.1234567',
+                }))
+                // Uncomment to add a memo (https://www.stellar.org/developers/learn/concepts/transactions.html)
+                // .addMemo(StellarSdk.Memo.text('Hello world!'))
+                .build();
+
+              // Sign this transaction with the secret key
+              // NOTE: signing is transaction is network specific. Test network transactions
+              // won't work in the public network. To switch networks, use the Network object
+              // as explained above (look for StellarSdk.Network).
+              transaction.sign(sourceKeypair);
+
+              // Let's see the XDR (encoded in base64) of the transaction we just built
+              console.log(transaction.toEnvelope().toXDR('base64'));
+
+              // Submit the transaction to the Horizon server. The Horizon server will then
+              // submit the transaction into the network for us.
+              server.submitTransaction(transaction)
+                .then(function (transactionResult) {
+                  console.log(JSON.stringify(transactionResult, null, 2));
+                  console.log('\nSuccess! View the transaction at: ');
+                  console.log(transactionResult._links.transaction.href);
+                })
+                .catch(function (err) {
+                  console.log('An error has occured:');
+                  console.log(err);
+                });
+            })
+            .catch(function (e) {
+              console.error(e);
+            });
+
         } else {
           res.end("ERROR: Destination account is not set up to accept " + TOKEN_NAME + ". SOLUTION: Set up destination account to accept " + TOKEN_NAME + ".");
         }
