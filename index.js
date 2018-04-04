e85f4c3c2c3a86bc624c25b05a5b110e0bbda3de
Brent Labasan <BrentLabasan@gmail.com>
2018-04-03 22:51:56 -0700

    pg moment

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var moment = require('moment');
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
