ba0c41d755974402ad7200f6170eec264f589313
Brent Labasan <BrentLabasan@gmail.com>
2018-04-03 22:53:02 -0700

    DB

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ client.connect();
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
+  console.log("BEGIN");
   for (let row of res.rows) {
+    console.log("ROW");
     console.log(JSON.stringify(row));
   }
+  console.log("END");
   client.end();
 });
 
