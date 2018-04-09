61f35f32c3a7834407f52192dc3c56a91ccc8189
Brent Labasan <BrentLabasan@gmail.com>
2018-04-09 11:52:30 -0700

    back

--- index.js
+++ index.js
@@ -241,5 +241,5 @@ app.post('/sends/create', function (req, res) {
 });
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("lcf-back is running at localhost:" + app.get('port'))
 })
