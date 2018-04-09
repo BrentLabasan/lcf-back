0d7e6d86eec5f062fe1582562eeec41a47594fa2
Brent Labasan <BrentLabasan@gmail.com>
2018-04-09 11:48:37 -0700

    unused routes

--- index.js
+++ index.js
@@ -34,13 +34,17 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+/*
 app.get('/', function (request, response) {
   response.send('Hello World!')
 })
+*/
 
+/*
 app.get('/send/create', function (request, response) {
   response.send('Creating a Send.')
 })
+*/
 
 const sendAmounts = {
   XLM: ".00001",
